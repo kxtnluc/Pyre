ea93095f77f70db02f7d610753da5cc5cba7ce5d
luc.juneau@mastercraft.com <luc.juneau@mastercraft.com>
2026-08-13 13:00:07 -0400

    sidebar-start

---
+++ Pyre.Element/Elements/Navbar/NavBarActionItemPyre.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Pyre.Element.Navbar
+{
+    public class NavBarActionItemPyre
+    {
+        public EventCallback CallbackAction { get; set; }
+        public RenderFragment? Icon { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ Pyre.Element/Elements/Navbar/NavBarTabPyre.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pyre.Element.Navbar
+{
+    public class NavBarTabPyre
+    {
+        public string Label { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public RenderFragment? Icon { get; set; }
+    }
+}
