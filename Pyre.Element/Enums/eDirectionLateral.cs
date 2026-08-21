4d1e9021e6c90a131df292a3eaf730b2e824b5bd
luc.juneau@mastercraft.com <luc.juneau@mastercraft.com>
2026-08-21 13:33:09 -0400

    touch-ups

---
+++ Pyre.Element/Enums/eDirectionLateral.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pyre.Element.Enums
+{
+    public enum E_DIRECTION_LATERAL
+    {
+        Center,
+        Left,
+        Right
+    }
+}
